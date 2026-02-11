14e20b1e08744d1fcf2a0391c1360b7951b6dbe5
rohanito006 <nyogbarohan@gmail.com>
2026-02-11 15:54:20 +0000

    test api

---
+++ Front-end/src/App.tsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='btn btn-small'>
+        test
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ Front-end/src/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import process from "process";
+
+const api = axios.create({
+    baseURL: process.env.REACT_PUBLIC_API_URL + "api/",
+})
+
+export default api;
