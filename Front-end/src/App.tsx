c0328d0a6350910d5e0266bff6a53f801584285c
rohanito006 <nyogbarohan@gmail.com>
2026-02-12 15:49:52 +0000

    test de la fonction getTansactions

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -27,20 +27,42 @@ function App() {
   useEffect(() => {
     getTransactions();
   }, []);
-  /*
+ 
     const amounts = transactions.map((t) => Number(t.amount) || 0)  ;
     const balance = amounts.reduce((acc, item) => acc + item, 0) || 0;
+
+    // income = revenus
     const income = amounts.filter((a) => a > 0).reduce((acc, item) => acc + item, 0) || 0;
+    // expense = dépenses
     const expense = amounts.filter((a) => a < 0).reduce((acc, item) => acc + item, 0) || 0;
-    */
+    const ratio = income > 0 ? Math.min((Math.abs(expense) / income) * 100, 100) : 0;
+
+    const formatDate = (dateString: string) => {
+      const d = new Date(dateString);
+      return d.toLocaleDateString('fr-FR', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+    }
+
+
   return (
     <>
       <Toaster />
-      <div className='btn btn-small flex items-center justify-center' onClick={getTransactions}>
-        test
-      </div>
       
-     
+
+       <div className="w-2/3 flex flex-col gap-2">
+        <div className="flex justify-between rounded-2xl border-2 border-warning border-dashed bg-warning/5 p-4">
+            <div className="flex flex-col gap-1">
+                <div></div>
+                <div className='stat-value'>{balance.toFixed(2)}</div>
+            </div>
+        </div>
+     </div>
+      
     </>
   )
 }
